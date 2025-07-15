c649c0de96e79ac63e1a5539e4345ffd3b5bc025
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-07-15 23:23:48 +0000

    Added the counter button and it changes dynamicaly when we click on it

--- Date_10jul/ReactStarting/counter-app/src/App.jsx
+++ Date_10jul/ReactStarting/counter-app/src/App.jsx
@@ -1,8 +1,19 @@
+import {useState} from "react";
+
+let state = {
+  count:0
+}
+
 function  App(){
+  const [count, setCount] = useState(0);
+  
+  function onclickHandler() {
+    setCount(count+1);
+  }
   return (
     <div>
-      Hi there
+      <button onClick={onclickHandler}> Counter: {count}</button>
     </div>
   )
 }
-export default  App
+export default App
