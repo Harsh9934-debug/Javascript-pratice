f502849046c5b0b3c7e79b14da631c69a65139fd
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-04 22:56:13 +0000

    Updated some of the code in the Tower Of Hanoi

--- pythonpratice/toh2.py
+++ pythonpratice/toh2.py
@@ -1,4 +1,16 @@
 def toh (n,start,aux,end):
     if n==1:
         print(f"Move the disk 1 form the {start} to {end}")
-    toh(n-1,)
+        return
+    toh(n-1,start,aux,end)
+    print(f"move the {n} disk from the {start} to {end}")
+    toh(n-1,aux,end,start)
+try:
+    n = int(input("Enter the number of the disks: "))
+    if(n==1):
+        print("Move the disk 1 form the {start} to {end}")
+    else:
+        print("Disks are moving in the following way")
+        toh(n,'A','B','C')
+except ValueError:
+    print("Enter the valid input")
