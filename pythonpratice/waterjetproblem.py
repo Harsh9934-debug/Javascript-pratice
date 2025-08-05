f4794ed023eaef46c010f30f0e73a24c585f459e
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-05 21:14:47 +0000

    Updated it for the pratice

--- pythonpratice/waterjetproblem.py
+++ pythonpratice/waterjetproblem.py
@@ -1,21 +1,19 @@
-from collections import deque
-
-def water_jug(j1, j2, target):
-    q, visited = deque([(0, 0)]), set()
-
-    while q:
-        a, b = q.popleft()
-        if (a, b) in visited: continue
-        visited.add((a, b))
-        print(f"Jug1: {a}, Jug2: {b}")
-        if a == target or b == target:
-            print(" Target reached  ")
-            return
-
-        q.extend([
-            (j1, b), (a, j2), (0, b), (a, 0),
-            (a - min(a, j2 - b), b + min(a, j2 - b)),
-            (a + min(b, j1 - a), b - min(b, j1 - a))
-        ])
-
-water_jug(4, 3, 2)
+from collections import defaultdict
+jug1, jug2, target = 4, 3, 2
+visited = defaultdict(bool)
+def solve(x, y):
+    if (x == target and y == 0) or (x == 0 and y == target):
+        print(f"({x}, {y})")
+        return True
+    if visited[(x, y)]:
+        return False
+    print(f"({x}, {y})")
+    visited[(x, y)] = True
+    return (solve(0, y) or  
+            solve(x, 0) or
+            solve(jug1, y) or
+            solve(x, jug2) or
+            solve(x + min(y, jug1 - x), y - min(y, jug1 - x)) or
+            solve(x - min(x, jug2 - y), y + min(x, jug2 - y)))
+print("Steps:")
+solve(0, 0)
