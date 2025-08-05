02231fb4d0270adaa826a7f739a0294906273a4f
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-05 17:22:04 +0000

    Continued some of the changes

--- pythonpratice/bestfirstsearch.py
+++ pythonpratice/bestfirstsearch.py
@@ -8,4 +8,10 @@ def bestfs(graph,start,goal):
         visited.add(node)
         print(node)
 
-        
+        if node == goal:
+            return True
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                pq.put((graph[node][neighbour],neighbour))
+    return False
+
