8296249e867e53f17aecfd3eaebe6ecab53bcd36
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-05 18:38:24 +0000

    Updated for the pratice

---
+++ pythonpratice/bfs2.py
@@ -0,0 +1,17 @@
+graph = {
+    '5':['3','7'],
+    '3':['2','4'],
+    '7':['8'],
+    '2':[],
+    '4':[],
+    '8':[],
+}
+visited = []
+queue= ['5']
+
+while queue:
+    node  = queue.pop(0)
+    if node not in visited:
+        print(node)
+        visited.append(node)
+        queue+=graph[node]
