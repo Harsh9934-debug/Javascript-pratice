0fc88edca295a699afb635e2314813d5344e9018
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-05 11:14:31 +0000

    Corrected my mistake in the program

--- pythonpratice/toh3.py
+++ pythonpratice/toh3.py
@@ -2,9 +2,9 @@ def toh(n,start,aux,end):
     if n==1:
         print(f"move the 1 disk from {start} to {end}")
         return
-    toh(n-1,start,aux,end)
-    print(f"move the {n} disk from the {start} to {aux}")
-    toh(n-1,aux,end,start)
+    toh(n-1,start,end,aux)
+    print(f"move the {n} disk from {start} to {end}")
+    toh(n-1,aux,start,end)
 try:
     n=int(input("Enter the number of disks"))
     if n <= 0:
