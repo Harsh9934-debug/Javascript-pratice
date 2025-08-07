568f515144aa9ffa6b3771b8ec9ea0ed8e90d462
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-07 23:59:10 +0000

    updated some of the changes

--- PythonExamPrep/7th.py
+++ PythonExamPrep/7th.py
@@ -5,3 +5,6 @@ courses = {'Python':'BCA', 'ACA':'BCOM','IBE':'BBA'}
 for k,v in courses.items():
     print("The key is ",k,"and the value is ",v)
 
+for k in courses.keys():
+    print("The key value is ",k)
+    
