7d32ad9114baee2d3ab567f74a2582e4025f8950
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-07 15:34:40 +0000

    Updated the first part of the code

--- PythonExamPrep/1st.py
+++ PythonExamPrep/1st.py
@@ -1,1 +1,15 @@
-#DATA TYPES IN THE PYTHON 
+#DATA TYPES IN PYTHON 
+class datatypes:
+    a = 0
+    if a>0:
+        print("The entered data is ",a)
+        print("The value entered is an positive interger")
+    elif a<0:
+        print("The entered data is ",a)
+        print("The value entered is an negative interger ")
+    elif a==0:
+        print("The entered data is: ",a)
+        print("The value entered is zero and it is an interger")
+    print("Data_type of a is ")
+    print(type(a))
+    print("****************************************************")
