4d39e5164a14e135be269861f201f02c51956e3b
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-08 00:27:22 +0000

    Updated some of the code

--- PythonExamPrep/13.py
+++ PythonExamPrep/13.py
@@ -4,3 +4,10 @@ def circle(r):
     area = 3.14*r*r
     print("The area of the cirsle is ",area)
 
+def rectangle(l,b):
+    area = l*b
+    print("The area of the rectangle is ",area)
+
+def triangle(b,h):
+    area = 0.5*b*h
+    print("The area of the triangle is ",area)
