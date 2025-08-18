4120e746a98b603c9261318fb94e03f1610f17e5
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-18 22:46:14 +0530

    Added  some of the changes

--- date_18aug/pratice_project_18Aug/src/App.jsx
+++ date_18aug/pratice_project_18Aug/src/App.jsx
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Dashboad } from '../components/Dashboad'
+import { Landing } from '../components/Landing'
 
 function App(){
   return (
-    <>
-        
-    </>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/dashboad" element ={<Dashboad/>} />
+           <Route path="/landing" element ={<Landing/>} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
