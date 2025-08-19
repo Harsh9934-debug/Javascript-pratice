cb4c08806f52a671b1254a68e2283f7b797d8e75
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-19 23:02:54 +0530

    This part the count section has been updated

--- date_18aug/pratice_project_18Aug/src/App.jsx
+++ date_18aug/pratice_project_18Aug/src/App.jsx
@@ -1,29 +1,35 @@
-import { BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom'
-import './App.css'
-import { Landing } from '../components/Landing'
-import { Dashboad } from '../components/Dashboad'
-
+import React, { useState } from "react"
 function App(){
+  const [count, setCount] = useState(0);
   return (
-      <BrowserRouter>
-        <Appbar />
-          <Routes>
-            <Route path="/dashboad" element ={<Dashboad/>} />
-            <Route path="/" element ={<Landing/>} />
-          </Routes>
-      </BrowserRouter>
+     <div>
+      <Count count = {count} />
+      <Buttons count={count} setCount={setCount} />
+     </div>
   )
 }
 
-function Appbar(){
-  const navigate = useNavigate();
+function Count({count}){
   return <div>
-       <div>
-        <button onClick={() => {navigate("/")}}>Landing page</button>
-        <button onClick={() => {navigate("/dashboad")}}>this is the dashboad</button>
-      </div>
+      {count}
+  </div>
+}
+function Buttons( {count,setCount} ){
+
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>
+    Increase
+    </button>
+    <button onClick={() => {
+      setCount(count - 1)
+    }}>
+    Decrease
+    </button>
   </div>
 
 }
+
 
 export default App
