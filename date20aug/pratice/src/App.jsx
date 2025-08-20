89d2cd3af61c9b8cd76b8c4fc83ab3f5610bb460
Harsh kumar gupta <harshkumargupta630@gmail.com>
2025-08-20 22:33:26 +0530

    Updated some of the changes

--- date20aug/pratice/src/App.jsx
+++ date20aug/pratice/src/App.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 
 const App = () => {
+
+  var user = "Harsh"
+
+  let changeuser= () => {
+    user = "Aryan"
+  }
+
   return (
     <div>
-      App
+      <h1>Username is {user}</h1>
+      <button onClick={changeuser}>Change user </button>
     </div>
   )
 }
