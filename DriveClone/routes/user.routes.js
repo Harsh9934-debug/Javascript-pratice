4ffcdff3424d8bd945b18f2963494cb02a4c8fa5
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-09-21 22:29:15 +0530

    Updated the landing page

--- DriveClone/routes/user.routes.js
+++ DriveClone/routes/user.routes.js
@@ -11,4 +11,11 @@ router.get('/register',(req,res) =>{
     res.render('register')
 })
 
+router.post('/register',async (req,res) =>{
+    const {name,email,password} = req.body
+    const user = new userModel({name,email,password})
+    await user.save()
+    res.redirect('/')
+})
+
 module.exports = router;
