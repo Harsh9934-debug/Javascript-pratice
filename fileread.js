4630d6893fad2d59e77e472db542423a756a9b32
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-11-25 18:27:47 +0530

    Deleated some of the file

--- fileread.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs")
-
-// fs is for the file system and this is used in reading the file 
-
-fs.readFile("a.txt","utf-8",function(err,data){
-    console.log(data)
-})
-   
--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const port = 3000
-const app = express();
-
-app.get('/',function(req,res){
-    res.send('Hello world ')
-})
-app.listen(port)
