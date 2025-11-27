bb89e16870ce4f8db268b9d7c149fcaad92b43b4
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-11-27 18:55:27 +0530

    Updated the changes

--- class.js
+++ class.js
@@ -12,6 +12,12 @@ class rectangle{
     }
 }
 
-const rect1=new rectangle(10,20,'blue');
+const rect1=new rectangle(10,5,'blue');  // this is how we create an object in the rectangle class 
+// rect1.height=10;
+// rect1.width=5;
+// rect1.color='blue';
+
+// this is how we access the methods of the class in the object also we can pass the parameters in the constructor
+
 console.log(rect1.area());
 console.log(rect1.colorInfo());
