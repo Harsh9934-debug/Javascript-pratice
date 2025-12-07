d22875f7ab566891ee85ef206706cc59e58b2702
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-12-07 19:49:50 +0530

    Updated the chanes

--- todoappwithsomeroutefeatures/index.js
+++ todoappwithsomeroutefeatures/index.js
@@ -5,6 +5,21 @@ app.get('/',(req,res)=>{
     res.send("Hello world");
 })
 
+app.get('/create-todo',(req,res)=>{
+    res.send("Create Todo Page");   
+})
+
+app.get('/read-todo',(req,res)=>{
+    res.send("Create Todo Page");   
+})
+app.get('/update-todo',(req,res)=>{
+    res.send("Create Todo Page");   
+})
+app.get('/delete-todo',(req,res)=>{
+    res.send("Create Todo Page");   
+})
+
+
 app.listen(3000,
     console.log("http://localhost:3000")
 )
