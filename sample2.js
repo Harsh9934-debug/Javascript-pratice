d6448a4d16e1f14c61c376e413dc65089b6fd38a
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2025-12-09 19:51:27 +0530

    Updated the changes

--- sample2.js
+++ sample2.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hi there!")
-})
+function oldenoughMiddleware(req, res, next) {
+    const age = req.query.age;
+    if (age >= 14) {
+        next();
+    } else {
+        res.send("You are not old enough to ride the roller coaster! Must be 14 or older.");
+    }
+}
 
-const PORT = 3000
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
-})
+app.get('/ride1', oldenoughMiddleware, (req, res) => {
+    res.send("Welcome to the roller coaster!");
+});
 
 
-
-
-
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
