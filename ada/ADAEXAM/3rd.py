6f0d1a3b54c6e512828ffea1d1a551158bd1902a
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-02-03 19:01:56 +0530

    Updated

---
+++ ada/ADAEXAM/3rd.py
@@ -0,0 +1,12 @@
+#write a python program to implement the tower of hanoi using recursion 
+
+def toh(n, source, destination, auxiliary):
+    if n==1:
+        print("Move disk 1 from source",source,"to destination",destination)
+        return
+    toh(n-1, source, auxiliary, destination)
+    print("Move disk",n,"from source",source,"to destination",destination)
+    toh(n-1, auxiliary, destination, source)
+    
+n=int(input("Enter the number of disks: "))
+toh(n,'A','B','C')
