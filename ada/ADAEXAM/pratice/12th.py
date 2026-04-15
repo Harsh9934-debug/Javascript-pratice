7796c1ff75b288e3f10e57d9684fa9c54b2bc622
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-04-15 16:37:15 +0530

    Updated the program

--- ada/ADAEXAM/pratice/12th.py
+++ ada/ADAEXAM/pratice/12th.py
@@ -2,6 +2,6 @@ def knapsack(capacity,weight,value):
     dp=[0]*(capacity+1)
     for weight,value in zip(weight,value):
         for curr_cap in range(capacity,weight-1,-1):
-            dp[curr_cap]=max(dp[curr_cap],dp[curr_cap-weight+value])
+            dp[curr_cap]=max(dp[curr_cap],dp[curr_cap-weight]+value)
     return dp[capacity]
-print("max value",knapsack(50,[10,20,30],[60,100,120]))
+print("Maximum Value",knapsack(50,[10,20,30],[60,100,120]))
