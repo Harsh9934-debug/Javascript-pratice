d78de444e9078d9d6fd94b1e76f09443b3bf0310
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-04-15 20:01:34 +0530

    Updated the chanegs

--- ada/ADAEXAM/pratice/8th.py
+++ ada/ADAEXAM/pratice/8th.py
@@ -2,10 +2,10 @@ g = [
     [0,1,999],
     [999,0,2],
     [3,999,0]
-    ]
+]
+
 for k in range(3):
     for i in range(3):
         for j in range(3):
-            g[i][j] = min(g[i][j], g[i][k] + g[k][j])
-
+            g[i][j] = min(g[i][j],g[i][k] + g[k][j])
 print(g)
