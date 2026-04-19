ad3c0c4b9a7f727e730329377849236fb51abd6b
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-04-19 22:17:53 +0530

    Updated the chaegs

--- informationTechnology/6th.php
+++ informationTechnology/6th.php
@@ -1,17 +1,8 @@
 <?php
-    setcookie("time", date('d-m-Y h:i:s'));
+setcookie("time", date("d-m-Y h:i:s"));
+
+if(isset($_COOKIE["time"]))
+    echo "Last visit: " . $_COOKIE["time"];
+else
+    echo "First visit";
 ?>
-<html>
-<body>
-    <center><h2>Last Visit Details</h2></center>
-    <center>
-    <?php
-        if (isset($_COOKIE["time"])) {
-            echo "Your last visit was on: " . $_COOKIE["time"];
-        } else {
-            echo "You are visiting for the first time!";
-        }
-    ?>
-    </center>
-</body>
-</html>
