b3a492813eeb934a1ac2e27cd48b70747ef6bd1a
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-04-20 17:25:25 +0530

    Updated

--- informationTechnology/6th.php
+++ informationTechnology/6th.php
@@ -1,8 +1,15 @@
 <?php
 setcookie("time", date("d-m-Y h:i:s"));
 
-if(isset($_COOKIE["time"]))
+if (isset($_COOKIE["time"]))
     echo "Last visit: " . $_COOKIE["time"];
 else
     echo "First visit";
 ?>
+
+
+
+
+
+
+
