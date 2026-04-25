a2af49144bc2f9822864f6c3d649e57273dec37b
Harsh Kumar Gupta <harshkumargupta630@gmail.com>
2026-04-25 22:28:28 +0530

    Updated

--- informationTechnology/2nd.php
+++ informationTechnology/2nd.php
@@ -1,14 +1,9 @@
 <?php
-echo "Multiplication Table ";
-echo "This is the table representaion form the 1 to 10 ";
-echo "<table border= '1'cellspacing='0' cellpadding='5'>";
+echo "Multiplication Table (1 to 10) <table border='1' cellspacing='0' cellpadding='5'>";
 for($i=1;$i<=10;$i++){
-  echo "<tr>";
-  for($j=0;$j<=10;$j++){
-    $result=$i*$j;
-    echo "<td> $i X $j = $result </td>";
-  }
- echo "</tr>";
+    echo "<tr>";
+    for($j=0;$j<=10;$j++) echo "<td>$i X $j = ".($i*$j)."</td>";
+    echo "</tr>";
 }
-echo "</table>"
+echo "</table>";
 ?>
